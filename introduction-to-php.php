cd8fc5387964b891e9428f11f6aab522a6aaaaf6
monk-2001 <65870933+monk-2001@users.noreply.github.com>
2020-05-24 16:43:59 -0400

    Create introduction-to-php.php

---
+++ introduction-to-php.php
@@ -0,0 +1,27 @@
+<?php   
+
+// task 1
+echo "Welcome to PHP!";
+echo "<hr style=\"margin 1rem 0\">";
+
+// task 2
+echo "PHP stands for \"Hypertext Preprocessor\"! ";
+echo "<hr style=\"margin 1rem 0\">";
+
+// task 3
+echo "<p>PHP stands for \"Hypertext Preprocessor\"! </p >";
+echo "<hr style=\"margin 1rem 0\">";
+
+// task 4
+echo "HTML - Stands for Hypertext Markup Language<br>CSS - Stands for Cascading Stylesheet<br>JS - Stands for JavaScript<br>PHP - Stands for Hypertext Preprocessor<br>SQL - Stands for Structural Query Language";
+echo "<hr style=\"margin 1rem 0\">";
+
+// task 5
+echo "<ul><li>HTML - Stands for Hypertext Markup Language</li>
+<li>CSS - Stands for Cascading Stylesheet</li>
+<li>JS - Stands for JavaScript</li>
+<li>PHP - Stands for Hypertext Preprocessor</li>
+<li>SQL - Stands for Structural Query Language</li>
+</ul>";
+
+?>
